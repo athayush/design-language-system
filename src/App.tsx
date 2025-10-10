8ee0d2a90963b6cf00d81fb6208f84a6d88d870f
Ankush <ak9108824@gmail.com>
2025-10-10 16:09:52 +0530

    fix: changed button variant in app.jsx file

--- src/App.tsx
+++ src/App.tsx
@@ -19,7 +19,7 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <Button variant="destructive" onClick={() => setCount((count) => count + 1)}>
+        <Button variant="error" onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </Button>
         <p>
