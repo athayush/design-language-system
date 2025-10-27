e04957c1219bb57552f4a2c0b1e69a19eec25fcd
Ankush <ak9108824@gmail.com>
2025-10-27 12:55:08 +0530

    fix: fixed static navbar issue

--- src/general/navigation/navbar/AthcareNavbar.tsx
+++ src/general/navigation/navbar/AthcareNavbar.tsx
@@ -1,29 +1,57 @@
-import { CirclePlus, CircleUserRound, Clock, House, Store } from "lucide-react";
+import { CircleUserRound, Clock, Home, Search } from "lucide-react";
 import { Navbar, NavbarItem } from "./Navbar";
+import { useState } from "react";
 
-function AthCareNavbar(){
-   return (
+function AthCareNavbar() {
+  const [active, setActive] = useState("home");
+
+  return (
     <Navbar>
-      <NavbarItem  icon={House}>
+      <NavbarItem icon={Home} isActive={active === "home"} onClick={() => setActive("home")}>
         Home
       </NavbarItem>
-      <NavbarItem  icon={Store}>
-        Store
+      <NavbarItem icon={Search} isActive={active === "search"} onClick={() => setActive("search")}>
+        Search
       </NavbarItem>
-      <NavbarItem icon={CirclePlus}>
-        Booking
+      <NavbarItem icon={Clock} isActive={active === "tracker"} onClick={() => setActive("tracker")}>
+        Tracker
       </NavbarItem>
-      <NavbarItem  icon={Clock}>
-        Trackers
-      </NavbarItem>
-      <NavbarItem
-       isActive
-        icon={CircleUserRound}
-      >
+      <NavbarItem icon={CircleUserRound} isActive={active === "profile"} onClick={() => setActive("profile")}>
         Profile
       </NavbarItem>
     </Navbar>
   );
 }
 
-export {AthCareNavbar}
+export { AthCareNavbar };
+
+// import { type LucideIcon } from "lucide-react";
+// import { Navbar, NavbarItem } from "./Navbar";
+
+// type NavItem = {
+//   label: string;
+//   icon: LucideIcon;
+//   href: string;
+//   isActive?: boolean;
+// }
+
+// type AthCareNavbarProps  ={
+//   items: NavItem[];
+// }
+
+// export function AthCareNavbar({ items }: AthCareNavbarProps) {
+//   return (
+//     <Navbar>
+//       {items.map((item) => (
+//         <NavbarItem
+//           key={item.href}
+//           href={item.href}
+//           icon={item.icon}
+//           isActive={item.isActive}
+//         >
+//           {item.label}
+//         </NavbarItem>
+//       ))}
+//     </Navbar>
+//   );
+// }
