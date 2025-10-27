1f59b4655b21b4a7d0c2e74ea8137318fda15be5
Ankush <ak9108824@gmail.com>
2025-10-27 12:45:33 +0530

    fix: changed imports with correct path

--- src/athcare/molecules/add-new-folder-dialog/AddNewFolderDialog.tsx
+++ src/athcare/molecules/add-new-folder-dialog/AddNewFolderDialog.tsx
@@ -1,5 +1,4 @@
 import { FolderPlusIcon } from "lucide-react";
-
 import { Button } from "@/components/ui/button";
 import {
   Dialog,
@@ -12,35 +11,39 @@ import {
 } from "@/components/ui/dialog";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
+import { Icon } from "@/design-tokens/icon/Icon";
 
 type AddNewFolderDialogProps = {
   open?: boolean;
-  onOpenChange?: (open: boolean) => void;
-  value?: string;
+  onOpenChange: (open: boolean) => void;
+  value: string;
   onValueChange: (value: string) => void;
   onCreate?: () => void;
   isLoading?: boolean;
 };
 
 export function AddNewFolderDialog({
-  open,
+  open = false,
   onOpenChange,
   value,
   onValueChange,
   onCreate,
   isLoading = false,
 }: AddNewFolderDialogProps) {
+  
   return (
     <Dialog open={open} onOpenChange={onOpenChange}>
       <DialogTrigger asChild>
-        <Button variant="outline" size="icon" className="aspect-square size-12 rounded-full">
-          <FolderPlusIcon />
+        <Button variant="ghost" >
+          <Icon className={{ container: "bg-tertiary/15 p-3 shadow-sm hover:g-tertiary/15 hover:p-3 hover:shadow-sm", icon: "size-6" }} icon={FolderPlusIcon} />
         </Button>
       </DialogTrigger>
+
       <DialogContent className="sm:max-w-md">
         <DialogHeader className="text-start">
           <DialogTitle>New Folder</DialogTitle>
         </DialogHeader>
+
         <div className="grid flex-1 gap-2 pt-2">
           <Label htmlFor="folderName" className="sr-only">
             Folder Name
@@ -52,10 +55,10 @@ export function AddNewFolderDialog({
             onChange={(e) => onValueChange(e.target.value)}
           />
         </div>
-        
+
         <DialogFooter className="mt-4 flex w-full justify-end gap-3">
           <DialogClose asChild>
-            <Button type="button" variant="outline">
+            <Button type="button" variant="outline" onClick={() => onOpenChange(false)}>
               Cancel
             </Button>
           </DialogClose>
